2db5d00ca82e9671886f12a7383f60d3c0c84831
PecharheadTTV <cpecha716@iwcc.edu>
2026-09-22 18:48:08 -0500

    Exercise 4-1 complete
    Exercise 4-1 complete

--- ch04_ex1/category_list.php
+++ ch04_ex1/category_list.php
@@ -2,8 +2,7 @@
 require_once('database.php');
 
 // Get all categories
-$query = 'SELECT * FROM categories
-          ORDER BY categoryID';
+$query = 'SELECT * FROM categories ORDER BY categoryID';
 $statement = $db->prepare($query);
 $statement->execute();
 $categories = $statement->fetchAll();
@@ -28,22 +27,31 @@ $statement->closeCursor();
             <th>Name</th>
             <th>&nbsp;</th>
         </tr>
-        
-        <!-- add code for the rest of the table here -->
-    
+        <?php foreach ($categories as $category) : ?>
+        <tr>
+            <td><?php echo $category['categoryName']; ?></td>
+            <td>
+                <form action="delete_category.php" method="post">
+                    <input type="hidden" name="category_id"
+                           value="<?php echo $category['categoryID']; ?>">
+                    <input type="submit" value="Delete">
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; ?>
     </table>
 
     <h2>Add Category</h2>
-    
-    <!-- add code for the form here -->
-    
+    <form action="add_category.php" method="post" id="add_category_form">
+        <label>Name:</label>
+        <input type="text" name="name">
+        <input id="add_category_button" type="submit" value="Add">
+    </form>
     <br>
     <p><a href="index.php">List Products</a></p>
-
-    </main>
-
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
-    </footer>
+</main>
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
+</footer>
 </body>
 </html>
